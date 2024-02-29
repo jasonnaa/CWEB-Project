7c1e6216900035b65e227566cad2a23476272635
Jason <jason.anderson.1999@gmail.com>
2024-02-28 20:58:53 -0600

    Added files

---
+++ GettingStarted/Test1/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            ViewBag.Message = "Your gallery page.";
+
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            ViewBag.Message = "Your info page.";
+
+            return View();
+        }
+
+        public ActionResult SimpleAddition()
+        {
+            ViewBag.Message = "Your addition page.";
+
+            return View();
+        }
+
+        public ActionResult VariableDatatype()
+        {
+            ViewBag.Message = "Your variables and data types page.";
+
+            return View();
+        }
+
+        public ActionResult ConditionsLoops()
+        {
+            ViewBag.Message = "Your conditional loop logic page.";
+
+            return View();
+        }
+
+        public ActionResult UserDetails()
+        {
+            ViewBag.Message = "Your user details page.";
+
+            return View();
+        }
+
+        public ActionResult Objects()
+        {
+            ViewBag.Message = "Your objects page.";
+
+            return View();
+        }
+
+        public ActionResult Arrays()
+        {
+            ViewBag.Message = "Your arrays page.";
+
+            return View();
+        }
+
+        public ActionResult Dictionary()
+        {
+            ViewBag.Message = "Your dictionary page.";
+
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            ViewBag.Message = "Your results page.";
+
+            return View();
+        }
+    }
+}
---
+++ GettingStarted/Test1/Controllers/MyFirstController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class MyFirstController : Controller
+    {
+        // GET: MyFirst
+        public string Index()
+        {
+            return "This is a default action..";
+        }
+
+        public string Welcome()
+        {
+            return "This is my own Welcome action method!!";
+        }
+    }
+}
